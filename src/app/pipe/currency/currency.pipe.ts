c7ee19848acecf9a3759582725ad51ca1c7a1cc4
Zolival <zolinak@gmail.com>
2022-01-24 17:34:39 +0100

    sort, currency fixed

--- src/app/pipe/currency/currency.pipe.ts
+++ src/app/pipe/currency/currency.pipe.ts
@@ -6,14 +6,35 @@ import { Product } from 'src/app/model/product';
 })
 export class CurrencyPipe implements PipeTransform {
 
-
   transform(value: number): any {
-    return new Intl.NumberFormat(new Intl.NumberFormat('hu-HU', {
-      style: 'currency',
-      currency: 'Huf',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 5
-       }).format(value))
-  }
+      if (!value) return value
 
+      return new Intl.NumberFormat('hu-HU', {
+        style: 'currency',
+        currency: 'Huf',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+      }).format(value)
+    }
+
+  }
+  /*
+  */
+
+  /*
+  transform(value: number): any {
+    if (value !== undefined && value !== null) {
+
+      return value.toLocaleString('hu-HU', {
+        style:"currency",
+        currency:"HUF",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+      } );
+
+    } else {
+      return
+    }
+  }
 }
+*/
