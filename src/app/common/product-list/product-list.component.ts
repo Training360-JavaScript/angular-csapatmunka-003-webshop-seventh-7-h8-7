624ca41b3df48f427a3b41d2f031bd382c37cb5a
BiroKrisz <biro.s.kr@gmail.com>
2022-01-25 12:21:53 +0100

    Removed the console logs.

--- src/app/common/product-list/product-list.component.ts
+++ src/app/common/product-list/product-list.component.ts
@@ -21,10 +21,8 @@ export class ProductListComponent implements OnInit {
   }
 
   onClickSort(data: string): void {
-    console.log("onClickSort started  data = ", data)
     this.sortKey = data;
     this.shorting(this.products, data);
-    console.log('onClickSort end')
   }
   onChangeSearch(event: Event, filterKey: string): void {
     this.search = (event.target as HTMLInputElement).value;
@@ -33,19 +31,15 @@ export class ProductListComponent implements OnInit {
 
   shorting(productList: Product[], key: string): Product[] {
 
-    console.log(`SortPipe started: key, keytemp = `, key, keyTemp )
     if (!Array.isArray(productList) || !key) return productList;
 
     if(key != keyTemp ) {
       keyTemp = key
-      console.log("keyTemp", keyTemp)
       return productList.sort( (a, b) => {
         if(typeof a[key] === "number" && typeof b[key] === "number" ) {
-          // console.log("number", a[key])
           return a[key] - b[key]
         }
         if(typeof a[key] == "string" && typeof b[key] == "string" ) {
-          // console.log("string", a[key])
           return a[key].toLowerCase().localeCompare(b[key].toLowerCase())
           }
         }
@@ -53,7 +47,6 @@ export class ProductListComponent implements OnInit {
       }
 
       if(key == keyTemp ) {
-        console.log("keyTemp", keyTemp);
         keyTemp = null;
 
         return productList.sort( (a, b) => {
