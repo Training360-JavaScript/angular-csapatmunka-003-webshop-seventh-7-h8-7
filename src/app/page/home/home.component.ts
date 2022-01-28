7a58b776454ebbe391ecf2484ffe6a19518b9496
vpeter1 <varga.peter.mobil@gmail.com>
2022-01-28 12:17:29 +0100

    home component fixed

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Product } from 'src/app/model/product';
 import { ProductService } from 'src/app/service/product.service';
@@ -6,15 +7,14 @@ import { ProductService } from 'src/app/service/product.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+ // productList: Product[] = this.productService.getAll();
 
-  productList: Product[] = this.productService.getAll()
+  productList$: Observable<Product[]> = this.productService.getAll2();
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
