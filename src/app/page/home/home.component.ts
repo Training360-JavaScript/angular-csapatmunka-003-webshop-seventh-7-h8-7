1840a13faf49586a11b38650430fe79c8c46ff8e
BiroKrisz <biro.s.kr@gmail.com>
2022-02-01 13:29:38 +0100

    updated the home page

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Product } from 'src/app/model/product';
+import { ConfigService } from 'src/app/service/config.service';
 import { ProductService } from 'src/app/service/product.service';
 
 @Component({
@@ -12,9 +13,13 @@ import { ProductService } from 'src/app/service/product.service';
 export class HomeComponent implements OnInit {
  // productList: Product[] = this.productService.getAll();
 
+  title: string = this.configService.appName
+
   productList$: Observable<Product[]> = this.productService.getAll2();
 
-  constructor(private productService: ProductService) {}
+  constructor(
+     private productService: ProductService,
+     private configService: ConfigService) {}
 
   ngOnInit(): void {}
 }
