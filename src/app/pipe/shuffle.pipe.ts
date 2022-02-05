cc0fe0713a458ec4ccb3b2cf7d958525e75f390a
Zolival <zolinak@gmail.com>
2022-02-05 17:15:29 +0100

    Admin page with new sticky table header, with add button included. For my own interest.**

--- src/app/pipe/shuffle.pipe.ts
+++ src/app/pipe/shuffle.pipe.ts
@@ -8,9 +8,11 @@ export class ShufflePipe implements PipeTransform {
 
   transform(list: Product[]): Array<Product> {
 
-  for (let i = list.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [list[i], list[j]] = [list[j], list[i]];
-  }
-  return list
+    if(!list || !Array.isArray(list)) return list
+
+    for (let i = list.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [list[i], list[j]] = [list[j], list[i]];
+    }
+    return list
 }}
